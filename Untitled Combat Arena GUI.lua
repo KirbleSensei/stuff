02b074f30c3e97c971343534c979b22f9e15e52e
KirbleSensei <74039131+KirbleSensei@users.noreply.github.com>
2023-01-28 20:32:12 +0300

    Update Untitled Combat Arena GUI.lua

--- Untitled Combat Arena GUI.lua
+++ Untitled Combat Arena GUI.lua
@@ -20,25 +20,6 @@ Plr.Idled:connect(function()
 -- // GUI Initialize  
 local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source"))()
 -- FUNCTIONS:
-local function customtwn(input, studspersecond, offset)
-    local char = game:GetService("Players").LocalPlayer.Character;
-    local input = input or error("input is nil");
-    local studspersecond = studspersecond or 1000;
-    local offset = offset or CFrame.new(0,0,0);
-    local vec3, cframe;
- 
-    if typeof(input) == "table" then
-        vec3 = Vector3.new(unpack(input)); cframe = CFrame.new(unpack(input));
-    elseif typeof(input) ~= "Instance" then
-        return error("wrong format used");
-    end;
-    
-    Time = (char.HumanoidRootPart.Position - (vec3 or input.Position)).magnitude/studspersecond;
-    local twn = game.TweenService:Create(char.HumanoidRootPart, TweenInfo.new(Time,Enum.EasingStyle.Linear), {CFrame = (cframe or input.CFrame) * offset});
-    twn:Play();
-    twn.Completed:Wait();
- end;
-
  local function GetClosestPlayer()
     local target = nil
     local distance = math.huge
@@ -55,13 +36,6 @@ local function customtwn(input, studspersecond, offset)
   
     return target
  end
-
--- // Remote Bypass
---[[for i,v in pairs(getgc(true)) do
-    if (type(v) == 'table' and rawget(v, 'Remote')) then
-        v.Remote.Name = v.Name
-    end
- end--]]
 --// Remove table dupes
 function RemoveTableDupes(tab)
 	local hash = {}
@@ -74,18 +48,10 @@ function RemoveTableDupes(tab)
 	end
 	return res
 end
-
 -- // Table creation
-local playernames = {}
-for i,v in pairs(Players:GetPlayers()) do
-    table.insert(playernames, v.Name)
-end
-Players.PlayerAdded:Connect(function(addedplayer)
-	table.insert(playernames, tostring(addedplayer))
-end)
 local npcs = {}
 for i,v in pairs(Workspace:GetDescendants()) do
-    if v:IsA("Model") and v:FindFirstChild("Humanoid") --[[and not table.find(playernames, v.Name)--]] then
+    if v:IsA("Model") and v:FindFirstChild("Humanoid") then
         table.insert(npcs, v.Name)
 		table.sort(npcs, function(a,b)
             return a < b
@@ -98,7 +64,7 @@ local Window =
     {
         Name = "Auto Farm",
         LoadingTitle = "W.I.P.",
-        LoadingSubtitle = "by Kirbles#0329",
+        LoadingSubtitle = "by Kirbles#5167",
         ConfigurationSaving = {
             Enabled = false,
             FolderName = nil,
@@ -124,17 +90,6 @@ local Window =
 
 local clienttab = Window:CreateTab("Client Features", 4483362458)
 
---[[local teleporttomarker = clienttab:CreateButton({
-    Name = "Teleport to Quest",
-    Callback = function()
-    for i,v in pairs(game:GetService("Workspace").Terrain:GetChildren()) do
-        if v:FindFirstChild("Marker") then
-            customtwn(v, 150, CFrame.new(0,0,-5))
-        end
-    end
-    end,
- })--]]
-
  local npcselect = clienttab:CreateDropdown({
     Name = "Farm Select",
     Options = RemoveTableDupes(npcs),
@@ -145,49 +100,6 @@ local clienttab = Window:CreateTab("Client Features", 4483362458)
     end,
  })
 
---[[local teleporttomarker = clienttab:CreateButton({
-    Name = "Teleport to Chests",
-    Callback = function()
-    for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
-        if v:FindFirstChild("Lock") then
-            customtwn(v.Lock, 150)
-            wait(1)
-            fireproximityprompt(v.Inside.ProximityPrompt, 10)
-            wait(2)
-        end
-    end
-    end,
- }) ]]--
-
---[[ local autoquest = clienttab:CreateToggle({
-    Name = "Auto Quest",
-    CurrentValue = false,
-    Flag = "autoquestflag",
-    Callback = function(autoquest)
-        if autoquest then
-            getgenv().autoquesttoggle = true
-            while getgenv().autoquesttoggle do
-                task.wait()
-                for i,v in pairs(game.Workspace:GetDescendants()) do
-                    if v:IsA("Model") and v.Name == getgenv().farmobject and v:FindFirstChild("Humanoid") then
-                        if v.Humanoid.Health > 0 and Plr.Character:WaitForChild("Humanoid").Health > 0 then
-                            Plr.Character.HumanoidRootPart.CFrame = v.WorldPivot
-                            repeat 
-                                task.wait()
-                                Plr.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0,0,6) -- * CFrame.Angles(math.rad(90),0,0)
-                                v.HumanoidRootPart.Anchored = true
-                                game:GetService("ReplicatedStorage").Communication.Events.TryPunch:FireServer()
-                            until v.Humanoid.Health <= 0 or getgenv().autofarmtoggle == false
-                        end
-                    end
-                end
-            end
-            else
-                getgenv().autoquesttoggle = false
-        end
-    end,
-}) ]]--
-
 	local autofarm = clienttab:CreateToggle({
 		Name = "Auto Farm",
 		CurrentValue = false,
@@ -197,14 +109,14 @@ local clienttab = Window:CreateTab("Client Features", 4483362458)
 				getgenv().autofarmtoggle = true
 				while getgenv().autofarmtoggle do
 					task.wait()
-					for i,v in pairs(game.Workspace:GetDescendants()) do
+					for i,v in pairs(Workspace:GetDescendants()) do
 						if v:IsA("Model") and v.Name == getgenv().farmobject and v:FindFirstChild("Humanoid") then
 							if v.Humanoid.Health > 0 and Plr.Character:WaitForChild("Humanoid").Health > 0 then
 								repeat 
 									task.wait()
 									Plr.Character:WaitForChild("HumanoidRootPart").CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0,0,5) -- * CFrame.Angles(math.rad(90),0,0)
 									--v.HumanoidRootPart.Anchored = true
-                                    game:GetService("ReplicatedStorage").Events.InputKey:FireServer("LMB")
+                                    					RS.Events.InputKey:FireServer("LMB")
 								until v.Humanoid.Health <= 0 or getgenv().autofarmtoggle == false
 							end
 						end
@@ -232,72 +144,18 @@ local clienttab = Window:CreateTab("Client Features", 4483362458)
 		Callback = function(autocounter)
 			if autocounter then
 				getgenv().autocounter = true
-                while getgenv().autocounter do
-                    task.wait(0.4)
-                    for i,v in pairs(GetClosestPlayer().Character.Torso:GetChildren()) do
-                        if v:IsA("Sound") and v.Name ~= "Block" or v.Name ~= "Alert Indicator" then
-                            game:GetService("ReplicatedStorage").Events.InputKey:FireServer(getgenv().selectedkey)
-                        end
-                    end
-                end
+                		while getgenv().autocounter do
+                    			task.wait(0.4)
+                    			for i,v in pairs(GetClosestPlayer().Character.Torso:GetChildren()) do
+                        			if v:IsA("Sound") and v.Name ~= "Block" or v.Name ~= "Alert Indicator" then
+                            				RS.Events.InputKey:FireServer(getgenv().selectedkey)
+                        				end
+                    				end
+                			end
 				else
 				    getgenv().autocounter = false
 			end
 		end,
 	})
 
-    --[[local freezenpcs = clienttab:CreateToggle({
-		Name = "Freeze Npcs",
-		CurrentValue = false,
-		Flag = "freezenpcflag",
-		Callback = function(freezenpc)
-			if freezenpc then
-				getgenv().freezenpctoggle = true
-				while getgenv().freezenpctoggle do
-					task.wait()
-					for i,v in pairs(game.Workspace:GetDescendants()) do
-						if v:IsA("Model") and v:FindFirstChild("Humanoid") and v.Name ~= Plr.Name then
-                            v.HumanoidRootPart.Anchored = true
-                        end
-                    end
-                end
-				else
-				    getgenv().freezenpctoggle = false
-                    for i,v in pairs(game.Workspace:GetDescendants()) do
-						if v:IsA("Model") and v:FindFirstChild("Humanoid") and v.Name ~= Plr.Name then
-                            v.HumanoidRootPart.Anchored = false
-                        end
-                    end
-			end
-		end,
-	})
-
-    local itemfarm = clienttab:CreateToggle({
-		Name = "Item Farm",
-		CurrentValue = false,
-		Flag = "itemfarmflag",
-		Callback = function(itemfarm)
-			if itemfarm then
-                getgenv().ItemFarm = true 
-                game:GetService("RunService").Stepped:connect(function()
-                pcall(function()
-            if ItemFarm then
-          for _,v in pairs(game.Workspace:GetDescendants()) do
-           if v:IsA("TouchTransmitter") then
-               firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
-               wait()
-               firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1)
-               end
-           end
-      end
-  end)
-end)
-				else
-				    getgenv().ItemFarm = false
-			end
-		end,
-	})]]--
-
-
-
-local Paragraph = clienttab:CreateParagraph({Title = "ddd", Content = "i love norweigan girls"})
+local Paragraph = clienttab:CreateParagraph({Title = "Note to self:", Content = "man im alone"})
