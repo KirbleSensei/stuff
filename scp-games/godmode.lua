2684818baa5e6722236e5e09759135d300b5cc3b
KirbleSensei <74039131+KirbleSensei@users.noreply.github.com>
2023-01-29 23:28:05 +0300

    Delete scp-games directory

--- scp-games/godmode.lua
+++
@@ -1,8 +0,0 @@
---// works for class d 
-while wait() do
-for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-   if v.ClassName == "BillboardGui" then
-       v:Destroy()
-   end
-end
-end
--- scp-games/gun mod.lua
+++
@@ -1,9 +0,0 @@
-for i,v in pairs(getgc(true)) do
-	if type(v) == "table" and rawget(v,"Clip") then
-	    rawset(v, "Clip", math.huge)
-	    rawset(v, "Ammo",  math.huge)
-	    rawset(v, "Spread", 0)
-	    rawset(v, "Damage",  math.huge)
-	    rawset(v, "TBS", 0.018)
-	end
-end
